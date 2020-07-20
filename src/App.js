26204550e9e11ba1a3b8d9e6dd4c644eb0ad1db8
bilalhadid <hadid0648@gmail.com>
2020-07-21 00:46:45 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Home } from "./component/Home";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <div className="App">
-        <Home />
-      </div>
-    </Routes>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
